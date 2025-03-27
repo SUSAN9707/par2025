e5d5f866b2bf8c152e16e06d4cb2041cb7a0a0d6
SUSAN9707 <144391303+SUSAN9707@users.noreply.github.com>
2025-03-27 14:02:21 -0400

    Add files via upload

---
+++ laboratorio/lab04/facturas-productos/src/main/java/com/laboratorio/facturas_productos/FacturasProductosApplication.java
@@ -0,0 +1,13 @@
+package com.laboratorio.facturas_productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturasProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturasProductosApplication.class, args);
+	}
+
+}
